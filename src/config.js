4e3491cb22ab5dc271ee2a02459268ff49c1f8e3
Charles <charlesnwoye2@gmail.com>
2025-03-14 02:17:22 +0100

    setup Flight Itinerary UI

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  apiBaseUrl: 'http://127.0.0.1:10000',
-  // apiBaseUrl: 'https://chasingcontinentsapi.onrender.com',
+  // apiBaseUrl: 'http://127.0.0.1:10000',
+  apiBaseUrl: 'https://chasingcontinentsapi.onrender.com',
 };
 
 export default config;
