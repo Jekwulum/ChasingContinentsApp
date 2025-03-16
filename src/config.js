c1bf153edb2320bd1fd7a91d703c990fe62db531
Charles <charlesnwoye2@gmail.com>
2025-03-16 12:52:28 +0100

    back to render

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
   // apiBaseUrl: 'http://127.0.0.1:10000',
-  // apiBaseUrl: 'https://chasingcontinentsapi.onrender.com',
-  apiBaseUrl: 'https://chasing-continents-api-dhhvg3c5b5ajhgcd.eastus2-01.azurewebsites.net/',
+  apiBaseUrl: 'https://chasingcontinentsapi.onrender.com',
+  // apiBaseUrl: 'https://chasing-continents-api-dhhvg3c5b5ajhgcd.eastus2-01.azurewebsites.net/',
 };
 
 export default config;
